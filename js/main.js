444a7c34508a567411f9581b1034a6b3ca8d3d9e
ModulanteHYL <13317634939@163.com>
2019-04-29 18:23:26 +0800

    增加导航栏效果

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
 setTimeout(function () {
     waitWrapper.classList.remove('active');
-}, 2000);
+}, 1000);
+window.onscroll=function(){
+    if(window.scrollY>0){
+        document.getElementById('topBar').classList.add('bg');
+    }else{
+        document.getElementById('topBar').classList.remove('bg');
+    }
+}
