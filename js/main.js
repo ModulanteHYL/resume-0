b18220012c687ad4d4b704426b66f0f3d2675fa5
ModulanteHYL <13317634939@163.com>
2019-04-30 15:43:44 +0800

    增加导航下拉菜单效果

--- js/main.js
+++ js/main.js
@@ -1,10 +1,31 @@
 setTimeout(function () {
     waitWrapper.classList.remove('active');
 }, 1000);
-window.onscroll=function(){
-    if(window.scrollY>0){
+window.onscroll = function () {
+    if (window.scrollY > 0) {
         document.getElementById('topBar').classList.add('bg');
-    }else{
+    } else {
         document.getElementById('topBar').classList.remove('bg');
     }
 }
+// let liTags = navMenu.getElementsByTagName('li');
+// for (let i = 0; i < liTags.length; i++) {
+//     liTags[i].onmouseenter = function (e) {
+//         let ul = e.target.firstElementChild;
+//         while (ul && ul.localName !== 'ul') {
+//             ul = ul.nextElementSibling;
+//         }
+//         if (ul) {
+//             ul.classList.add('active');
+//         } else { }
+//     }
+//     liTags[i].onmouseleave = function (e) {
+//         let ul = e.target.firstElementChild;
+//         while (ul && ul.localName !== 'ul') {
+//             ul = ul.nextElementSibling;
+//         }
+//         if (ul) {
+//             ul.classList.remove('active');
+//         } else { }
+//     }
+// }
